208549ec57b8f42f0986ed94395a5c969d95297f
Paritosh <tiwariparitosh9999@gmail.com>
2021-02-26 01:24:20 +0530

    Factory Pattern added

---
+++ LeetArraySecondQuestion/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LeetArraySecondQuestion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
